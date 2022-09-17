30c712fc70545307d4c31986fa5e9267365e5448
hsears0930 <hsears0930@gmail.com>
2022-09-17 14:49:34 -0700

    printf in C correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - printf function
  * return: 0
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
