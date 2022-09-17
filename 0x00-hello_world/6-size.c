5dcd0199434622899549f154ec850b9c47f44fdf
hsears0930 <hsears0930@gmail.com>
2022-09-17 15:38:33 -0700

    sizeof and printf for byte sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ int main(void)
 	float floatType;
 
 	printf("Size of a char: %lu byte(s)\n", sizeof(charType));
-	printf("Size of a int: %lu byte(s)\n", sizeof(intType));
+	printf("Size of an int: %lu byte(s)\n", sizeof(intType));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(longintType));
 	printf("Size of a long long int: %lu byte(s)\n", sizeof(llintType));
 	printf("Size of a float: %lu byte(s)\n", sizeof(floatType));
