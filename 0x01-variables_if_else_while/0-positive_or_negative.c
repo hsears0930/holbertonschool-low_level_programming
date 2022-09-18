8cfabdfa62ebc3002273bfc3fbd42e3d00887cac
hsears0930 <hsears0930@gmail.com>
2022-09-18 16:05:22 -0700

    first if loop in C

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * printf - displays the assigned text
+ * scanf - scans input according to designated format
+ * %d: for decimal integer
+ * return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	scanf("%d", &n);
+	if (n > 0)
+		printf("%d is positive", n);
+	else if (n < 0)
+		printf("%d is negative", n);
+	else if (n == 0)
+		printf("%d is zero", n);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+This dir is for project 1 which is writing variables and loops in C
