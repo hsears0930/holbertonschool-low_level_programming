baf4db2233eeac0ab2e83acf14cd1130c95f5bc1
hsears0930 <hsears0930@gmail.com>
2022-09-18 11:27:55 -0700

    printf statement in C

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * printf - this function prints a statement
+ * return - 0
+ */
+int main(void);
+{
+	printf("_putchar");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This dir is for learning how to write functions and nested loops in C
