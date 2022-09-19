001162355bbdfa834de864886853823608f96d5f
hsears0930 <hsears0930@gmail.com>
2022-09-19 10:39:07 -0700

    base 16 counting

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: putchar to count all hexidecimals in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char(x);
+	char(N);
+
+	for (x = 30; x <= 39; x++)
+	{
+		putchar (x);
+	}
+	for (N = 61; N <= 66; N++)
+	{
+		putchar (N);
+	}
+	putchar ('\n');
+
+	return (0);
+}
