308996da340a1e5f3b460901129b8e47ef024297
hsears0930 <hsears0930@gmail.com>
2022-09-19 09:47:32 -0700

    counting to 10 in 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: printing numbers through ten
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x = 0
+
+		while
+			(x < 10);
+				(x++);
+				return (0);
+}
