ee9389d4344ee2ff49100153f2db45c74272b585
hsears0930 <hsears0930@gmail.com>
2022-09-19 10:10:38 -0700

    counting to ten in ten with putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: putchar to show zero thru ten
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char(x);
+
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x);
+	}
+	putchar ('\n');
+
+	return (0);
+}
