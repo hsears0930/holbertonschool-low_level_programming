73c234bbdc4c2a66f8124bc6c1082fe09461da48
hsears0930 <hsears0930@gmail.com>
2022-09-19 11:53:41 -0700

    counting with commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: putchar to print one thru nine with commas
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar (x);
+		if (x != '9')
+		{
+			putchar (',');
+			putchar (' ');
+		}
+		putchar ('\n');
+		return (0);
+	}
+}
