f3301539263e60f5c41e1c2115b94f053b1d0a2f
hsears0930 <hsears0930@gmail.com>
2022-09-18 17:52:53 -0700

    putchar small alph

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/** 
+ * printf - displays the desired text
+ * scanf - reads the desired variable 
+ * %d - decimal value 
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0);
+			n = rand() - RAND_MAX / 2;
+			printf("
---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * putchar - writes single characters to the stdout
+ *return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x\n);
+}
