c8ee254809212ec392bc351c3f997802646895c4
hsears0930 <hsears0930@gmail.com>
2022-09-19 10:28:15 -0700

    putchar tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: putchar to print tebahpla
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char(x);
+
+	for (x = 'z'; x >= 'a'; x--)
+	{
+		putchar (x);
+	}
+	putchar ('\n');
+
+	return (0);
+}
