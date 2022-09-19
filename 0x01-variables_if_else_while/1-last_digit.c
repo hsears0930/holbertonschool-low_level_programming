3ba933adcc7a2f4ccb3ce87583ad438634e7ee18
hsears0930 <hsears0930@gmail.com>
2022-09-19 12:44:47 -0700

    last digit count

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,6 +22,7 @@ int main(void)
 		printf("Last digit of %i is %i and is 0\n", n, x);
 	else if (x < 6 && x > 0)
 		printf("Last digit of %i is %i and is less than 6 and is not 0\n", n, x);
-		return (0);
+
+	return (0);
 }
 
