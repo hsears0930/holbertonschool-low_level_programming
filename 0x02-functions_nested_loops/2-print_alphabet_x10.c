57acc431fc0e0cd2b8d16c50e66a898c6edd8bf9
hsears0930 <hsears0930@gmail.com>
2022-09-21 12:36:08 -0700

    proj 0x02 task 2 print alph x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - main function
+ *
+ * Description: printing a-z ten times
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int z = 0;
+
+	while (z++ <= 10)
+	{
+		char x;
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			putchar (x);
+		}
+		putchar ('\n');
+	}
+}
--- 0x02-functions_nested_loops/2-print_aplhabet_x10.c
+++
@@ -1,7 +0,0 @@
-#include "main.h"
-
-/**
- * main - entry point
- *
- * Return: Always 0
- */
