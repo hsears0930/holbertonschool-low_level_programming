27329e4c99e5703763b48a5c729c3a40b0911473
hsears0930 <hsears0930@gmail.com>
2022-09-23 07:43:18 -0700

    proj 0x04 task 6, print lines

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*print_line - main function
+*Description: print a line n times or just newline
+*@n: the number of times a line is printed
+*Return: line or newline on success
+*/
+void print_line(int n)
+{
+	int x;
+
+	if (n >= 1)
+	{
+		for (x = 0; x <= n; x++)
+
+		_putchar('_');
+	}
+	_putchar('\n');
+}
